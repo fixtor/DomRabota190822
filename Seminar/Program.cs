74b57d3544f43212f9e96709a6e3835fb4efde15
Igor_U <fixtor75@gmail.com>
2022-08-19 20:52:40 +0300

    Что-то начали делать

---
+++ Seminar/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] CreateRandomArray(int lenght, int start, int end)
+{
+    int[] RandomArray = new int[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
+}
+
+int[] myRandomArray = CreateRandomArray(num, min, max);
